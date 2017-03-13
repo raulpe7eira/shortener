dd73bc86eed8fe0fe5f10b5653fe766cd1741a06
Raul Pereira <raul.pereira@gmail.com>
2017-03-13 18:20:23 -0300

    Remove PRD ENV VARGS

--- scripts/prd-server.sh
+++ scripts/prd-server.sh
@@ -1,9 +1,6 @@
 # shortener/scripts/prd-server.sh
 
 echo '\n\033[1;33m> PRODUCTION ENVIRONMENT MODE\033[m'
-export NODE_ENV='prd'
-export MONGOLAB_USER='ushortener'
-export MONGOLAB_PASSWORD='#us%40abcd1234$'
 
 echo '\n\033[0;33mProduction preparing...\033[m\n'
 node 'scripts/prd-data.js' --timeout 10000
