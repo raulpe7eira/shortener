e027cf5afcd6957e43055c09c8f93dab7fbfe33b
Raul Pereira <raul.pereira@gmail.com>
2017-03-13 18:24:57 -0300

    Correct port prd

--- config/env/prd.js
+++ config/env/prd.js
@@ -4,7 +4,7 @@ module.exports = {
 
 	env: 'prd',
 	db: `mongodb://${process.env.MONGOLAB_USER}:${process.env.MONGOLAB_PASSWORD}@ds129610.mlab.com:29610/shortener`,
-	port: 8080,
+	port: process.env.PORT,
 	debug: false
 
 };
