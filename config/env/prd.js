f48d7db35bfaf148a942509bcb8f9ea0fdefd550
Raul Pereira <raul.pereira@gmail.com>
2017-03-13 18:02:38 -0300

    Change address prd

--- config/env/prd.js
+++ config/env/prd.js
@@ -5,7 +5,7 @@ module.exports = {
 	env: 'prd',
 	debug: false,
 
-	address: 'gentle-crag-99464.herokuapp.com',
+	address: '107.22.213.25',
 	port: 80,
 
 	db: `mongodb://${process.env.MONGOLAB_USER}:${process.env.MONGOLAB_PASSWORD}@ds129610.mlab.com:29610/shortener`
