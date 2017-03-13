df4e67436f6fcf6c7b2d8243e0faf426e5548992
Raul Pereira <raul.pereira@gmail.com>
2017-03-13 18:00:11 -0300

    Log in prd environment

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = require('./config/express')(config.port);
 
 require('./config/database')(config.db);
 
+console.log(config.env);
+console.log(config.address);
+console.log(config.port);
+
 module.exports = require('http').createServer(app).listen(config.port, config.address, () => {
 
 	config.debug && console.log('\n[shortener-%s %s] URL Short Server! Inicializado em %s:%d ...',
