d59b0c7194962639ca9428141a307d7a05586b1b
Raul Pereira <raul.pereira@gmail.com>
2017-03-14 11:19:23 -0300

    Frontend new feature

--- config/express.js
+++ config/express.js
@@ -13,6 +13,11 @@ module.exports = () => {
     app.use(express.static(path.join(__dirname, '../bower_components')));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+  		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+		next();
+	});
 	app.enable('trust proxy');
 
 	// backend
--- public/js/shorten.js
+++ public/js/shorten.js
@@ -1,57 +1,112 @@
-+function ($) {
++(($) => {
     'use strict';
 
-    $('#btn-shorten').on('click', () => {
-        $.ajax({
-            url: `/shortener/create/?${$.param({ url: $('#url-field').val(), CUSTOM_ALIAS: $('#alias-field').val() })}`,
-            contentType: 'application/json',
-            type: 'POST',
-            success: (data) => {
+    let shortenURL = () => {
+        $.post(`/shortener/create/?${$.param({ url: $('#url-field').val(), CUSTOM_ALIAS: $('#alias-field').val() })}`)
+            .done((res) => {
                 let resultHTML =
                     `<div class="alert alert-success alert-dismissible" role="alert">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                         <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                        <span class="sr-only">Success:</span> Your Short URL <a href="${data.url}" class="alert-link">${data.url}</a> is DONE!
-                    </div>`
+                        <span class="sr-only">Success:</span> Your Short URL <a href="${res.url}" class="alert-link">${res.url}</a> is DONE!
+                    </div>`;
                 $('#response').html(resultHTML);
                 $('#response').hide().fadeIn('slow');
-            },
-            error: (err) => {
+            })
+            .fail((err) => {
+                if (err.responseJSON) {
+                    let resultERR =
+                        `<div class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            <span class="sr-only">Error:</span> <strong>${err.responseJSON.err_code} - ${err.responseJSON.description}</strong>
+                        </div>`;
+                    $('#response').html(resultERR);
+                    $('#response').hide().fadeIn('slow');
+                }
+            });
+    };
+
+    let retrieveURL = () => {
+        $.get(`/shortener/u/${$('#alias-field').val()}`)
+            .done((res) => {
+                console.log(`ok: ${res}`);
+                document.write(JSON.stringify(res));
+            })
+            .fail((err) => {
+                console.log(`err: ${err}`);
+                if (err.responseJSON) {
+                    let resultERR =
+                        `<div class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            <span class="sr-only">Error:</span> <strong>${err.responseJSON.err_code} - ${err.responseJSON.description}</strong>
+                        </div>`;
+                    $('#response').html(resultERR);
+                    $('#response').hide().fadeIn('slow');
+                }
+            });
+    };
+
+    let topTen = () => {
+        $.get('/shortener/top10')
+            .done((res) => {
                 let resultHTML =
-                    `<div class="alert alert-danger alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                        <span class="sr-only">Error:</span> <strong>${err.responseJSON.err_code} - ${err.responseJSON.description}</strong>
-                    </div>`
-                $('#response').html(resultHTML);
-                $('#response').hide().fadeIn('slow');
-            }
-        });
+                    `<div class="panel panel-default table-responsive">
+                        <table class="table table-striped table-hover table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>#</th><th>Short URL</th><th>Original URL</th><th>Retrieved</th>
+                                </tr>
+                            </thead>
+                            <tbody>`;
+                $.each(res.top10, (i, item) => {
+                    resultHTML +=
+                        `<tr>
+                            <th scope="row">${i+1}</th>
+                            <td><a href="/shortener/u/${item.alias}">http://shortener/u/${item.alias}</a></td>
+                            <td>${item.url}</td><td>${item.retrieved}</td>
+                        </tr>`;
+                });
+                resultHTML +=
+                            `<tbody>
+                        </table>
+                    </div>`;
+                $('#top10').html(resultHTML);
+                $('#top10').hide().fadeIn('slow');
+            });
+    };
+
+    $(document).on('click.bs.button.data-api', '[data-toggle^="button"]', () => {
+        if ($('input:checked')[0].id === 'shorten') {
+            $('#url-field').prop('disabled', false);
+            $('#url-field').val('');            
+        } else {
+            $('#url-field').prop('disabled', true);
+            $('#url-field').val('http://shortener/u/');
+        }
+    });
+
+    $('#btn-shorten').on('click', () => {
+        if ($('input:checked')[0].id === 'shorten') {
+            shortenURL();
+        } else {
+            retrieveURL();
+        }
     });
 
     $('#btn-refresh').on('click', () => {
-        $.ajax({
-            url: '/shortener/top10',
-            contentType: 'application/json',
-            type: 'GET',
-            success: (data) => {
-                let resultHTML = '<div class="panel panel-default table-responsive"><table class="table table-striped table-hover table-condensed"><thead><tr><th>#</th><th>Short URL</th><th>Original URL</th><th>Retrieved</th></tr></thead><tbody>';
-                $.each(data.top10, (i, item) => {
-                    resultHTML += `<tr><th scope="row">${i+1}</th><td><a href="/shortener/u/${item.alias}">http://shortener/u/${item.alias}</a></td><td>${item.url}</td><td>${item.retrieved}</td></tr>`;
-                });
-                resultHTML += '<tbody></table></div>';
-                $('#top10').html(resultHTML);
-                $('#top10').hide().fadeIn('slow');
-            }
-        });
+        topTen();
     });
 
     $(window).on('load', () => {
         $('#btn-refresh').click();
     });
 
-}(jQuery);
+})(jQuery);
